b84135be35ca17e61861e2047aa6288908649c5d
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-12-19 19:28:53 +0000

    bare template

---
+++ Main/index.php
@@ -0,0 +1,41 @@
+<?php
+
+?>
+<!doctype html>
+<!-- ::>>.Notes: 
+
+
+-->
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href=""/>  
+  </head>
+  <body>
+<!-- Menu Scetion -->
+    <section>
+      <svg></svg>
+      <div></div>
+    </section>
+<!-- Menu Scetion -->
+
+<!-- Game Window -->
+
+<!-- Game Window -->
+  
+  
+<!-- Scripts Section -->  
+    <script src=""></script> 
+    <script src=""></script>
+<!-- Scripts Section -->    
+  </body>
+</html>
+
+
+<?php
+
+?>
+<!-- ::>>.Notes: 
+
+
+-->
